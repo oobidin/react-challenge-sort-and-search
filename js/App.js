4e7b3ae3b17ffaac0f58c1df80747c99392448d6
Oleg Obidin <o.obidin@nmdsystems.com>
2016-03-31 21:00:20 +0300

    added skeletons for another components + added jQuery and data request

--- js/App.js
+++ js/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import SearchBar from './components/SearchBar';
 import ToolBar from './components/ToolBar';
+import UserData from './components/UserData';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
+    
+    this.state = {
+        active: 0,
+        searchValue: '',
+        data: [],
+        activeData: []
+    };
   }
   
-  doSearch(e) {
-      console.log(e);
+  componentDidMount() {
+      $.getJSON('data.json', (data) => {
+          this.setState({
+              data: data
+          });
+      })
+  }
+  
+  doSearch(val) {
+      this.setState({
+         searchValue: val 
+      });
+      console.log(this);
   }
   
   doSort(by) {
@@ -18,8 +37,9 @@ export default class App extends Component {
   render() {
     return (
       <div className="app container-fluid">
-        <SearchBar update={this.doSearch}/>
+        <SearchBar update={this.doSearch.bind(this)}/>
         <ToolBar buttonClick={this.doSort}/>
+        <UserData searchValue={this.state.searchValue}/>
       </div>
     );
   }
